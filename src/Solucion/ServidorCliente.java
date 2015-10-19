4864025bbcd413fc122de4c5322f4ac2feed5ff6
LauraCortes <la.cortes10@uniandes.edu.co>
2015-10-19 09:43:20 -0500

    Verifica cifrado servidor

--- src/Solucion/ServidorCliente.java
+++ src/Solucion/ServidorCliente.java
@@ -16,6 +16,7 @@ import javax.crypto.Cipher;
 
 
 
+import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -26,8 +27,9 @@ import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
 public class ServidorCliente 
 {
 	public static String RTA_AFIRTMATIVA="RTA:OK";
-	
-	private static double NUM1=2;
+	public static String ALGORITMO="RSA";
+	public static String MAC="HMACMD5";
+	private static double NUM1=Math.random();
 	private static double NUM2;
 	
 	public static void main(String[] args) throws Exception 
@@ -67,7 +69,7 @@ public class ServidorCliente
 				System.out.println("Servidor: " + fromServer);
 
 				//AlGORTIMOS QUE SE VAN A UTILIZAR
-				fromUser ="ALGORITMOS:RSA:HMACMD5";
+				fromUser ="ALGORITMOS:"+ALGORITMO+":"+MAC;
 				System.out.println("Cliente: " + fromUser);
 				escritor.println(fromUser);
 				fromUser = "";
@@ -82,8 +84,8 @@ public class ServidorCliente
 					escritor.println(fromUser);
 
 					//FLUJO DE BYTES DEL CERTIFICADO
-					Certificado enviar = new Certificado();
-					byte [] mybye = enviar.certificado.getEncoded();
+					Certificado certificadoCliente = new Certificado();
+					byte [] mybye = certificadoCliente.certificado.getEncoded();
 					
 					//ENVIO AL SERVIDOR DESDE EL CLIENTE
 					servidor.getOutputStream().write(mybye);
@@ -98,7 +100,6 @@ public class ServidorCliente
 						//NUMERO ALEATORIO
 						fromServer = lector.readLine();
 						NUM2=Double.parseDouble(fromServer.split(":")[0]);
-						System.out.println("Num2: "+NUM2);
 						System.out.println("Servidor: " + fromServer);
 
 						//RECIBE CERTIFICADO DEL SERVIDOR
@@ -130,49 +131,52 @@ public class ServidorCliente
 							String privateKEY = lector.readLine();
 							System.out.println("Servidor: " + privateKEY);
 
-							
-							if(privateKEY!=null)
+							//DESCIFRA EL NUMERO QUE ENVIO EL SERVIDOR
+							Cipher desCifrado = Cipher.getInstance(ALGORITMO);
+							desCifrado.init(Cipher.DECRYPT_MODE, certificadoServidor.getPublicKey());
+							byte[] cipheredText = new byte[privateKEY.length()/2];
+							for (int i = 0 ; i < cipheredText.length ; i++) 
 							{
-								//ACEPTA LLAVE SERVIDOR
+								cipheredText[i] = (byte) Integer.parseInt(privateKEY.substring(i*2,(i+1)*2), 16);
+							}
+							byte [] clearText = desCifrado.doFinal(cipheredText);
+							String s3 = new String(clearText);
+							double respuestaServidor=Double.parseDouble(s3);
+							
+							if(privateKEY!=null && NUM1==respuestaServidor)
+							{
+								//ACEPTA EL CIFRADO DEL SERVIDOR
 								fromUser =RTA_AFIRTMATIVA;
 								System.out.println("Cliente: " + fromUser);
 								escritor.println(fromUser);
 								
-								
-								Cipher cifrado = Cipher.getInstance("RSA");
-								cifrado.init(Cipher.ENCRYPT_MODE, enviar.getPriv());
-
-								byte [] envio1 = (NUM1 + "").getBytes();
-							    byte[] funciona =  cifrado.doFinal(envio1);
-							    String s2 = new String (funciona);
-
-								System.out.println("Cliente: Este es el mensaje cifrado " + s2);
-
-								escritor.println(s2);
-
-								
-								//Etapa 4
-								
+								//CIFRA EL NUMERO 2
+								Cipher cipher = Cipher.getInstance(ALGORITMO);
+								byte [] num2 = (Double.toString(NUM2)).getBytes();
+								cipher.init(Cipher.ENCRYPT_MODE, certificadoCliente.getPriv());
+								byte [] cipheredNum2 = cipher.doFinal(num2);
+								fromUser = "";
+								for (int i = 0 ; i < cipheredNum2.length ; i++) {
+									String g = Integer.toHexString(((char)cipheredNum2[i])&0x00ff);
+									fromUser += (g.length()==1?"0":"") + g;
+								}
+						        System.out.println("Cliente: "+fromUser);
+						        escritor.println(fromUser);
+								//ETAPA 4:
 								if ((fromServer = lector.readLine()) != null  )
-								{
+								{		
 									
-									
-									System.out.println("Cliente: Este es el mensaje cifrado " + s2);
-
 
 								}
 								else
 								{
 									System.out.println("Paila");
-
 									break;
-
 								}
-							    
 							}
 							else
 							{
-								//NO ACEPTA LA LLAVE DEL SERVIDOR
+								//NO ACEPTA EL CIFRADO DEL SERVIDOR
 								fromUser ="RTA:ERROR";
 								System.out.println("Error  Cliente: " + fromUser);
 								escritor.println(fromUser);
